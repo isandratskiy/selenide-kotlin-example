59a1c560b724b889ad93f3c72c4844d3e3efbdb3
isandratskiy <i.sandrackii@gmail.com>
2019-11-17 15:24:46 +0200

    cleanup

--- src/main/kotlin/io/github/isandratskiy/driver/RemoteWebDriverFactory.kt
+++ src/main/kotlin/io/github/isandratskiy/driver/RemoteWebDriverFactory.kt
@@ -3,58 +3,51 @@ package io.github.isandratskiy.driver
 import com.codeborne.selenide.Configuration.*
 import java.lang.System.*
 import com.codeborne.selenide.Configuration.browser
+import io.github.isandratskiy.driver.WebDriverFactory.Companion.RemoteBrowser.*
 
 class WebDriverFactory {
     companion object {
         fun createWebDriverInstance() = when (getBrowserProperty()) {
-            RemoteBrowser.Firefox.toString() -> RemoteBrowser.Firefox.configure()
-            RemoteBrowser.Chrome.toString() -> RemoteBrowser.Chrome.configure()
+            Firefox.toString().toLowerCase() -> Firefox.configure()
+            Chrome.toString().toLowerCase() -> Chrome.configure()
             else -> LocalBrowser.Chrome.configure()
         }
+
+        private enum class RemoteBrowser {
+            Chrome {
+                override fun configure() {
+                    setRemoteCapabilities()
+                    setRemoteInstance()
+                    browser = "chrome"
+                }
+            },
+            Firefox {
+                override fun configure() {
+                    setRemoteCapabilities()
+                    setRemoteInstance()
+                    browser = "firefox"
+                }
+            };
+
+            abstract fun configure()
+        }
+
+        enum class LocalBrowser {
+            Chrome {
+                override fun configure() {
+                    browser = ChromeDriverProvider::class.qualifiedName
+                }
+            };
+
+            abstract fun configure()
+        }
     }
-}
-
-private enum class RemoteBrowser {
-    Chrome {
-        override fun configure() {
-            println("http://${getRemoteHub()}:4444/wd/hub")
-            setRemoteCapabilities()
-            setRemoteInstance()
-            browser = "chrome"
-        }
-    },
-    Firefox {
-        override fun configure() {
-            setRemoteCapabilities()
-            setRemoteInstance()
-            browser = "firefox"
-        }
-    };
-
-    abstract fun configure()
-}
-
-enum class LocalBrowser {
-    Chrome {
-        override fun configure() {
-            browser = ChromeDriverProvider::class.qualifiedName
-        }
-    };
-
-    abstract fun configure()
 }
 
 private fun getBrowserProperty() = getProperty("browser")
 
-private fun getRemoteHub(): String {
-    return when {
-        getProperty("remoteHub").isNullOrEmpty() -> "0.0.0.0"
-        else -> getProperty("remoteHub")
-    }
-}
-
 private fun setRemoteInstance() {
-    remote = "http://${getRemoteHub()}:4444/wd/hub"
+    remote = "http://0.0.0.0:4444/wd/hub"
 }
 
 private fun setRemoteCapabilities() {
