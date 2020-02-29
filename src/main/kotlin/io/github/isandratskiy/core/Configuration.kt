b2ddf82fa80748bdd5314bff1c76df089c41d5cf
isandratskiy <i.sandrackii@gmail.com>
2020-02-29 14:34:04 +0200

    added gradle retry plugin

--- src/main/kotlin/io/github/isandratskiy/core/Configuration.kt
+++ src/main/kotlin/io/github/isandratskiy/core/Configuration.kt
@@ -1,7 +1,7 @@
 package io.github.isandratskiy.core
 
-import com.codeborne.selenide.Configuration
-import com.codeborne.selenide.logevents.SelenideLogger
+import com.codeborne.selenide.Configuration.*
+import com.codeborne.selenide.logevents.SelenideLogger.*
 import io.qameta.allure.selenide.AllureSelenide
 
 class Configuration {
@@ -11,11 +11,11 @@ class Configuration {
         }
 
         private fun setSelenideConfiguration() {
-            SelenideLogger.addListener("allure", AllureSelenide().screenshots(true).savePageSource(false))
-            Configuration.baseUrl = "https://the-internet.herokuapp.com"
-            Configuration.browserSize = "1920x1080"
-            Configuration.fastSetValue = true
-            Configuration.timeout = 10000
+            addListener("allure", AllureSelenide().screenshots(true).savePageSource(false))
+            baseUrl = "https://the-internet.herokuapp.com"
+            browserSize = "1920x1080"
+            fastSetValue = true
+            timeout = 10000
         }
     }
 }
